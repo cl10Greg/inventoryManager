a662a117b1fe9ae690886660e0c3520d9ad6b69a
cl10Greg <cl10Greg@gmail.com>
2014-01-12 17:03:53 -0500

    New inventory name
    New inventory db name

--- Software/Electronic Inventory Manager/Electronic Inventory Manager/mainFrm.vb
+++ Software/Electronic Inventory Manager/Electronic Inventory Manager/mainFrm.vb
@@ -1,3 +1,24 @@
 ﻿Public Class mainFrm
 
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        'End 
+        End
+
+        'Prompt for saving any unsaved information
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        'Name the new database
+        Dim dbName As String
+        dbName = InputBox("Enter the name for the database.", "New Database", "newDB")
+
+        'Check for the valid name
+        If dbName = "newDB" Or dbName = "" Then
+            MsgBox("Error, please enter a valid name.")
+        Else
+            MsgBox("Create the DB")
+            'Make a new newDB with the correct name
+
+        End If
+    End Sub
 End Class
